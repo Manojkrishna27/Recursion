7d9152e46e20036e3473b56a0e51a314b14a02e0
Manojkrishna27 <manojkrishna2725@gmail.com>
2025-12-22 15:11:53 +0530

    reverse every word

--- test.py
+++ test.py
@@ -1,6 +1,16 @@
-for i in range(1,10):
-    if i==5:
-        break
-    print(i)
+def binarysearch(arr,target):
+    high=len(arr)-1
+    low=0
+    while low <=high:
+        mid=(low+high)//2
 
-print("manoj")
+        if arr[mid]==target:
+            return mid
+        elif arr[mid]<target:
+            low=mid+1
+        else:
+            high=mid-1
+    return -1
+arr=[1,2,3,4,5,6,7]
+target=4
+print(binarysearch(arr,target))
